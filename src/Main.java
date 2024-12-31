a5a24393b19b4cd3c807b549f592e049f02a1786
Дмитрий <gockindmitrij0@gmail.com>
2024-12-31 14:17:32 +0300

    Finish homework lines

--- src/Main.java
+++ src/Main.java
@@ -21,5 +21,6 @@ public class Main {
         System.out.println("Задача 3");
         fullName = "Иванов Семён Семёнович";
         System.out.println("Данные Ф.И.О. сотрудника - " + fullName.replace('ё','е'));
+
     }
 }
